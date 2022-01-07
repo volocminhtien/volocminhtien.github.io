0f1378e79804b1711779c61275f9a3dbe2a48999
Tiendeptrai <77500544+volocminhtien@users.noreply.github.com>
2022-01-07 13:30:35 +0700

    Update content.js

--- Sneaker/TreEm/content.js
+++ Sneaker/TreEm/content.js
@@ -134,6 +134,45 @@ $('#nav-logIn').onclick = ()=>{
   Modal.style.display = 'block'
   $('.nav').style.display = 'none'
 }
+//Filter Price
+function showVal(newVal){
+  $(".price1").innerHTML= `${newVal}K`;
+}
+function showVal2(newVal){
+  $(".price2").innerHTML= ` ${newVal}K `;
+}
+window.onload = function(){
+  slideOne();
+  slideTwo();
+}
+
+let sliderOne = document.getElementById("slider-1");
+let sliderTwo = document.getElementById("slider-2");
+let displayValOne = document.getElementById("range1");
+let displayValTwo = document.getElementById("range2");
+let minGap = 0;
+let sliderTrack = document.querySelector(".slider-track");
+let sliderMaxValue = document.getElementById("slider-1").max;
+
+function slideOne(){
+  if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+      sliderOne.value = parseInt(sliderTwo.value) - minGap;
+  }
+  displayValOne.textContent = sliderOne.value;
+  fillColor();
+}
+function slideTwo(){
+  if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+      sliderTwo.value = parseInt(sliderOne.value) + minGap;
+  }
+  displayValTwo.textContent = sliderTwo.value;
+  fillColor();
+}
+function fillColor(){
+  percent1 = (sliderOne.value / sliderMaxValue) * 100;
+  percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+  sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+}
 
 
 
